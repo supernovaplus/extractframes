4784b85ab58d878c8ec47abeff413391ccd7b9a5
Nova+ <cszonalt@gmail.com>
2020-07-19 23:23:49 +0300

    update

--- index.js
+++ index.js
@@ -4,29 +4,33 @@ const spawn = require('child_process').spawn;
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const {videosPath, screenshotPath} = require("./options.json");
 const express = require('express');
-const { dir } = require("console");
+const { query } = require("express");
 const app = express();
 const port = 3000;
 
-function getAllFiles (_path, _localpath = "/") {
+let isScanningActive = false;
+let scanProgress = 0;
+
+function getAllFiles (_path, subfolder = "/") {
     const files = [];
     fs.readdirSync(_path, { withFileTypes: true }).forEach(file=>{
         if(file.isDirectory()) {
-            files.push(...getAllFiles(path.join(_path, file.name), _localpath + file.name + "/"))
+            files.push(...getAllFiles(path.join(_path, file.name), subfolder + file.name + "/"))
         }else{
-            files.push({subfolder: _localpath, name: file.name})
+            files.push({subfolder, name: file.name})
         }
     })
     return files;
 }
 
+//scan folders and subfolders and filter leaving mp4 only
 const fileList = getAllFiles(videosPath).filter(v=>v.name.endsWith("mp4"))
 
 app.use('/videos', express.static(videosPath))
 app.use('/screenshots', express.static(screenshotPath))
 app.use('/', express.static(path.join(__dirname, "public")));
 
-app.get('/items.json', (req, res) => {
+app.get('/videos.json', (req, res) => {
     let limit = +req.query.limit;
     let currentPage = +req.query.page;
 
@@ -45,34 +49,62 @@ app.get('/items.json', (req, res) => {
         },
         files: fileList.slice((currentPage - 1) * limit,  currentPage * limit) 
     })
-  })
+})
 
-app.listen(port, () => console.log(`Express server @ http://localhost:${port}`))
-// makeScreenshots2();
-// async function makeScreenshots2(){
-//     // for (let i = 0; i < 100; i++) {
-//     for (let i = 0; i < fileList.length; i++) {
-//         const file = fileList[i];
-//         if(!fs.existsSync(`${screenshotPath}${file.name}.jpg`)){
-//             await new Promise(resolve => {
-//                 const ffmpeg = spawn(ffmpegPath, [
-//                     "-ss", "00:00:10",
-//                     "-i", `${videosPath + (file.dir ? file.dir + "\\" : "") + file.name}`,
-//                     "-frames", "1",
-//                     "-vf", `select='not(mod(n,1500))',scale=480:270,tile=8x1`,
-//                     // "-vf", `select='gt(scene,0.4)',scale=160:120,tile`,
-//                     "\"" + screenshotPath + file.name + ".jpg" + "\""
-//                 ], {shell: true});
-//                 ffmpeg.stderr.on('data', (data) => {
-//                     console.log(`X ${data}`);
-//                 });
-//                 ffmpeg.on('close', (code) => {
-//                     console.log("done")
-//                     resolve();
-//                 });
-//             });
-//         }
-//     }
+app.get('/scanning.json', (req, res) => {
+    if(req.query.start !== undefined && !isScanningActive){
+        res.json({
+            "scanstatus": 0,
+            "message": "scan started"
+        })
+        makeScreenshots2();
+    }else if(isScanningActive){
+        res.json({
+            "scanstatus": 1,
+            "message": "scan in progress",
+            "progress": scanProgress + "/" + fileList.length
+        })
+    }else{
+        res.json({
+            "scanstatus": 2,
+            "message": "scan finished"
+        })
+    }
+})
 
-//     console.log("screenshotting finished")
-// }
+async function makeScreenshots2(){
+    isScanningActive = true;
+    scanProgress = 0;
+
+    for (let i = 0; i < fileList.length; i++) {
+        const file = fileList[i];
+        const input = videosPath + (file.subfolder ? file.subfolder.replace(/\//g,"\\") : "") + file.name
+        if(!fs.existsSync(`${screenshotPath}${file.name}.jpg`)){
+            await new Promise(resolve => {
+                const ffmpeg = spawn(ffmpegPath, [
+                    "-ss", "00:00:02",
+                    "-i", input,
+                    "-frames", "1",
+                    "-vf", `select='not(mod(n,1500))',scale=480:270,tile=8x1`,
+                    // "-vf", `select='gt(scene,0.4)',scale=160:120,tile`,
+                    "\"" + screenshotPath + file.name + ".jpg" + "\""
+                ], {shell: true});
+                ffmpeg.stderr.on('data', (data) => {
+                    console.log(`X ${data}`);
+                });
+                ffmpeg.on('close', (code) => {
+                    console.log("done")
+                    scanProgress++;
+                    resolve();
+                });
+            });
+        }else{
+            scanProgress++;
+        }
+    }
+
+    isScanningActive = false;
+}
+
+
+app.listen(port, () => console.log(`Express server @ http://localhost:${port}`));
--- public/main.js
+++ public/main.js
@@ -12,7 +12,7 @@ Vue.component('video-list-page', {
         fetchList(pageid = 1){
             if(!pageid || pageid < 1 && pageid > this.maxPage) return;
 
-            fetch("items.json?page="+pageid).then(res=>res.json()).then(res=>{
+            fetch("videos.json?page="+pageid).then(res=>res.json()).then(res=>{
                 if(res && res.status === 200 && res.files.length > 0){
                     this.fileList = res.files;
                     this.fileCount = res.fileCount;
@@ -30,8 +30,12 @@ Vue.component('video-list-page', {
         playVideo(url){
             console.log(videoPlayer)
             videoPlayer.src = "videos/"+url;
-            videoPlayer.play();
-            videoPlayer.currentTime = 100;
+            
+            videoPlayer.onloadeddata = function() {
+                videoPlayer.currentTime = (videoPlayer.duration * 0.25);
+                videoPlayer.play();
+            };
+
         }
     },
     beforeDestroy: function () {
@@ -43,7 +47,7 @@ Vue.component('video-list-page', {
         videoPlayer.volume = 0.1;
     },
     template: `<div>
-    <video width="1024" height="720" controls id="videoPlayer" type="video/mp4"></video>
+    <video width="1024" height="720" controls id="videoPlayer" type="video/mp4" loop></video>
     <page-change-block @fetchList="fetchList" v-bind:props="{currentFileListPage, nextPage, maxPage, fileCount}"/>
     <h2 v-if="error">Problem: {{error}}</h2>
     <ul id="itemsList">
@@ -66,37 +70,63 @@ Vue.component('page-change-block', {
         }
     },
     template: `<div>
-        Total Files: {{ this.props.fileCount }} | Current Page: {{ this.props.currentFileListPage }}
+        Total Files: {{ this.props.fileCount }} | Current Page: {{ this.props.currentFileListPage }}/{{ this.props.maxPage }}
         <input type="button" value="Back" v-on:click="() => emit(this.props.currentFileListPage - 1)" :disabled="this.props.currentFileListPage <= 1">
         <input type="button" value="Next" v-on:click="() => emit(this.props.nextPage)" :disabled="!this.props.nextPage">
         <input type="number" v-on:keyup.enter="(e) => emit(+e.target.value)" :placeholder="'Max Page: '+this.props.maxPage" :max="this.props.maxPage">
     </div>`
 });
 
-Vue.component('navigate-page', {
-    data: function(){
-        return {
-            links: [
-                {name: "Files", id: 0},
-                {name: "Page 2", id: 1},
-                {name: "Page 3", id: 2}
-            ]
-        };
-    },
-    props: ["props"],
-    methods:{
-        emitPage(pageid){
-            this.$emit("selectPage", pageid)
-        }
-    },
+Vue.component('home-page', {
     template: `<div>
-        <a v-for="(link, index) in this.links" :key="index" v-on:click="emitPage(link.id)" href="#">- {{ link.name }}</a>
+    Home
     </div>`
 });
 
-Vue.component('demo-page-one', {
+Vue.component('file-scanner', {
+    data: function(){
+        return {
+            interval: null,
+            scanInProgress: true,
+            message: "Not started",
+            progress: 0
+        }
+    },
+    methods:{
+        checkProgress(initializeScan = false){
+            fetch("scanning.json" + (initializeScan ? "?start" : "")).then(res=>res.json()).then(res=>{
+                if(res.scanstatus === 0 || res.scanstatus === 1){
+                    if(!this.interval){
+                        this.interval = setInterval(()=>{
+                            this.checkProgress();
+                        },2000);
+                    }
+                    this.scanInProgress = true;
+                    this.message = res.message;
+                    this.progress = res.progress || -1;
+                }else{
+                    if(this.interval){
+                        clearInterval(this.interval);
+                        this.interval = null;
+                    }
+                    this.scanInProgress = false;
+                    this.message = res.message;
+                }
+            }).catch(err=>{
+                console.log(err)
+                this.message = "error";
+            })
+        },
+    },
+    mounted: function(){
+        this.checkProgress();
+    },
+    beforeDestroy: function(){
+        if(this.interval) clearInterval(this.interval);
+    },
     template: `<div>
-    demo-page-one
+    <input type="button" value="start scanning" v-on:click="checkProgress(true);" :disabled="this.scanInProgress">
+    {{this.message}} | {{this.progress}}
     </div>`
 });
 
@@ -110,20 +140,29 @@ Vue.component('demo-page-two', {
 const app = new Vue({
     el: '#app',
     data: {
-        contentPage: 1
+        links: [
+            {name: "Home", id: 0},
+            {name: "Video List", id: 1},
+            {name: "Folder Scanner", id: 2},
+            {name: "Page 3", id: 3}
+        ],
+        currentContentPage: 0
     },
     methods: {
         selectPage(pageid){
-            this.contentPage = pageid;
+            this.currentContentPage = pageid;
         }
     },
 
     template: `<div>
-        <navigate-page @selectPage="selectPage" v-bind:props="{contentPage}"/>
+        <div id="links">
+            <a v-for="(link, index) in this.links" :key="index" v-on:click="selectPage(link.id)" href="#">#{{ link.name }}</a>
+        </div>
         <div>
-            <video-list-page v-if="contentPage === 0"/>
-            <demo-page-one v-if="contentPage === 1"/>
-            <demo-page-two v-if="contentPage === 2"/>
+            <home-page v-if="currentContentPage === 0"/>
+            <video-list-page v-if="currentContentPage === 1"/>
+            <file-scanner v-else-if="currentContentPage === 2"/>
+            <demo-page-two v-else-if="currentContentPage === 3"/>
         </div>
     </div>`
 })
